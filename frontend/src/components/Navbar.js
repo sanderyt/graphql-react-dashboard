805eec9a60930c0daf6cf79f33a73b219585c239
Sander van Rijsoort <hallo@svanrijsoort.nlsgit config --global user.email hallo@svanrijsoort.nl>
2020-02-24 18:38:59 +0100

    copied some styling from other project

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
       <div className="navbar">
-        <h2>React app</h2>
+        <Link to="/">
+          <h2>React app</h2>
+        </Link>
+        <Link to="/login">
+          <button className="btn">Login</button>
+        </Link>
+        <Link to="/register">
+          <button className="btn btn--cta">Register</button>
+        </Link>
       </div>
     </header>
   );
